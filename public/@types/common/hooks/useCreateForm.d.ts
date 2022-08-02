6415b79fcda8ec2c366347147473f17dc22205c9
Nikita Yushchenko <nikita.yushenko@soft-fx.lv>
2022-08-02 11:42:49 +0300

    handleContinue changes; wmftypes update

--- public/@types/common/hooks/useCreateForm.d.ts
+++ public/@types/common/hooks/useCreateForm.d.ts
@@ -1,8 +1,13 @@
+declare type OnSubmit = (data: any) => Promise<any>;
+interface SubmitCallback {
+    onSuccess: (result: any) => void;
+    onError: (error: any, fn: (error: any) => void) => void;
+}
 declare const useCreateForm: (yupObject: any, mode?: any) => {
     isLoad: any;
     control: import("react-hook-form").Control<import("react-hook-form").FieldValues, object>;
     errors: import("react-hook-form").FieldErrorsImpl<import("react-hook-form").DeepRequired<import("react-hook-form").FieldValues>>;
-    submit: (onSubmit: any, callbacks: any) => Promise<void>;
+    submit: (onSubmit: OnSubmit, callbacks?: SubmitCallback) => Promise<void>;
     isValid: boolean;
     getValues: import("react-hook-form").UseFormGetValues<import("react-hook-form").FieldValues>;
 };
