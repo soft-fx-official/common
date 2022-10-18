cbf70c14b918ab80c5c7f1e60e3c356c74a39b55
Stanislav Okunev <stanislav.okunev@softnet.by>
2022-10-18 22:45:49 +0300

    0.8.3

--- public/@types/common/components/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { IModuleLoader } from './ModuleLoader';
-export { ModuleLoader } from './ModuleLoader';
-export type { IErrorBoundary } from './ErrorBoundary';
-export { ErrorBoundary } from './ErrorBoundary';
--- public/@types/common/inits/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IInitApp, initApp } from './app';
-import { IInitLocale, initLocale } from './locales';
-import { IInitState, IInitStateR, initState } from './state';
-import { IInitStorage, IInitStorageR, initStorage } from './storage';
-export { initApp, initLocale, initState, initStorage };
-export type { IInitApp, IInitLocale, IInitState, IInitStateR, IInitStorage, IInitStorageR };
