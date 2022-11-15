e91ccad0fb65fb1244c81f2052f65009ca14feee
Ilya Fedotov <ilia.fedotov@softnet.by>
2022-11-15 22:37:23 +0400

    fix models and update types

--- src/models/Module.ts
+++ src/models/Module.ts
@@ -3,3 +3,14 @@ export interface IModule {
   name: string
   module: string
 }
+export class Module implements IModule {
+  url: string
+  name: string
+  module: string
+
+  constructor(url: string, name: string, module: string) {
+    this.url = url
+    this.name = name
+    this.module = module
+  }
+}
