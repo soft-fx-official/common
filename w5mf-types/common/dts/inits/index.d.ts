f255d6f82d1f2e7697afc95c0866c9a661a50ae9
Ilya Fedotov <ilia.fedotov@softnet.by>
2022-11-23 19:29:56 +0400

    added URL params parsing and moved code context

--- w5mf-types/common/dts/inits/index.d.ts
+++ w5mf-types/common/dts/inits/index.d.ts
@@ -1,7 +1,7 @@
 import { IInitApi, IInitApiR, initApi } from './api';
-import { IInitApp, initApp } from './app';
+import { IInitApp, initApp, TinitApp } from './app';
 import { IInitLocale, initLocale } from './locales';
 import { IInitState, IInitStateR, initState } from './state';
 import { IInitStorageR, initStorage } from './storage';
 export { initApi, initApp, initLocale, initState, initStorage };
-export type { IInitApi, IInitApiR, IInitApp, IInitLocale, IInitState, IInitStateR, IInitStorageR };
+export type { IInitApi, IInitApiR, IInitApp, IInitLocale, IInitState, IInitStateR, IInitStorageR, TinitApp, };
